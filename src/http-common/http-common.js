6e14a536f1ed7f8af7ec4adf62d969592a986dd7
shkemb123 <shkembi12345@gmail.com>
2021-11-29 20:39:35 +0100

    Commons server added

--- src/http-common/http-common.js
+++ src/http-common/http-common.js
@@ -1,1 +1,8 @@
-//Commons
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "http://worktrial.herokuapp.com/api/v1",
+    headers: {
+        "Content-type": "application/json"
+    }
+});
